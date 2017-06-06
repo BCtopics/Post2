5c2976ac5736754050ce2b6ac742ab386ce824a3
Bradley Gilmore <bradleygilmoreefuf@yahoo.com>
2017-06-06 13:46:48 -0600

    Implement JSON conversion into model

--- Post2/Post2/Post.swift
+++ Post2/Post2/Post.swift
@@ -10,12 +10,21 @@ import Foundation
 
 struct Post {
     
+    //MARK: - Keys
+    
+    fileprivate let userKey = "username"
+    fileprivate let textKey = "text"
+    fileprivate let timestampKey = "timestamp"
+    
     //MARK: - Internal Properties
     
     let username: String
     let text: String
     let timestamp: TimeInterval
     let identifier: UUID
+    
+    
+    //MARK: - Initializers
     
     init(username: String, text: String, identifier: UUID? = UUID(), timestamp: TimeInterval? = Date().timeIntervalSince1970 ) {
         
@@ -27,5 +36,19 @@ struct Post {
         self.timestamp = timestamp
         
     }
+    
+    init?(jsonDictionary: [String: Any], identifier: String) {
+        
+        guard let username = jsonDictionary[userKey] as? String,
+            let text = jsonDictionary[textKey] as? String,
+            let timestamp = jsonDictionary[timestampKey] as? Double,
+            let identifier = UUID(uuidString: identifier) else { return nil }
+        
+        self.username = username
+        self.text = text
+        self.timestamp = TimeInterval(floatLiteral: timestamp)
+        self.identifier = identifier
+    }
+    
     
 }
